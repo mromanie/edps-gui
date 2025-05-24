a8e06a1b254469dcda6f9f996403bf1ab2e5115c
mromanie <mromanie@eso.org>
2025-05-24 13:28:33 +0200

    Update and rename setup.sh to postCreateCommand.sh

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo >> .profile
-echo "alias start_gui='/home/user/bin/start_gui.sh'" >> .profile
-echo "alias check_gui='ps -ef | grep panel'" >> .profile 
-
-echo >> .bashrc
-echo "alias start_gui='/home/user/bin/start_gui.sh'" >> .bashrc
-echo "alias check_gui='ps -ef | grep panel'" >> .bashrc
