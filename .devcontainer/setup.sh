4365654e64f89ff1719b8de8fcc731f649c17ca6
mromanie <mromanie@eso.org>
2025-05-24 08:10:18 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,3 +3,7 @@
 echo >> .profile
 echo "alias start_gui='/home/user/bin/start_gui.sh'" >> .profile
 echo "alias check_gui='ps -ef | grep panel'" >> .profile 
+
+echo >> .bashrc
+echo "alias start_gui='/home/user/bin/start_gui.sh'" >> .bashrc
+echo "alias check_gui='ps -ef | grep panel'" >> .bashrc
