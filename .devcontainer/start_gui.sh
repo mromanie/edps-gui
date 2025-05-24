20c512ad4b76afdf506bdf04da194f3c31d210fe
mromanie <mromanie@eso.org>
2025-05-24 20:38:47 +0200

    Update start_gui.sh

--- .devcontainer/start_gui.sh
+++ .devcontainer/start_gui.sh
@@ -4,4 +4,4 @@ repo_name=$(basename $GITHUB_REPOSITORY)
 
 panel serve $gui --plugins edpsgui.pdf_handler --address 0.0.0.0 --port $port --allow-websocket-origin='*' >& /home/user/edps-gui.log &
 
-echo "echo Start the GUI at: https://${CODESPACE_NAME}-${port}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/${repo_name}"
+echo "Start the GUI at: https://${CODESPACE_NAME}-${port}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/${repo_name}"
