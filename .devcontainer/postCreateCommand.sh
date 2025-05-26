438e2303705b0005291bef61964caaec8d5796d5
mromanie <mromanie@eso.org>
2025-05-26 15:15:39 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,4 @@
 # Script to be run once when the container is created.
 
 #pip install -r requirements_notebooks.txt
-python -m pip install --no-cache-dir --upgrade -r requirements_notebooks.txt
+python3.11 -m pip install --no-cache-dir --upgrade -r requirements_notebooks.txt
