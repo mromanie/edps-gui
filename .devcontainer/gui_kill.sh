2fb67415391ce21ed88e0cfa60e11ab07d977aca
mromanie <mromanie@eso.org>
2025-05-27 21:03:39 +0200

    Update gui_kill.sh

--- .devcontainer/gui_kill.sh
+++ .devcontainer/gui_kill.sh
@@ -2,4 +2,4 @@
 
 job_number=`ps -efww | grep edps-gui.py | grep panel | awk '{print $2}'`
 
-if [[ -n $job_number ]]; then kill $job_number ; fi
+if [[ -n $job_number ]]; then kill -9 $job_number ; fi
