ebf5d31247ff3facffd651f0bfd9d33153596318
mromanie <mromanie@eso.org>
2025-05-28 14:14:32 +0200

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,4 @@
 # Script to be run once when the container is created.\
 
 /usr/local/python/current/bin/python3.11 -m pip install -r /home/user/setup_files/requirements_notebooks.txt
-
+ln -s /home/user/EDPS_data /workspaces/$(basename $GITHUB_REPOSITORY)/EDPS_data
