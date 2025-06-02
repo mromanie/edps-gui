3c3e2c1b359728c6dd56e437f4f6f8fdf4a2a937
mromanie <mromanie@eso.org>
2025-06-02 11:47:27 +0200

    Delete .devcontainer directory

--- .devcontainer/gui_kill.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-job_number=`ps -efww | grep edps-gui.py | grep panel | awk '{print $2}'`
-
-if [[ -n $job_number ]]; then kill -9 $job_number ; fi
--- .devcontainer/postCreateCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to be run once when the container is created.\
-
-/usr/local/python/current/bin/python3.11 -m pip install -r /home/user/setup_files/requirements_notebooks.txt
-
-#Needed to download files to the local machine: /workspaces is where JupyterLab allows to download the data from
-ln -s /home/user/EDPS_data /workspaces/$(basename $GITHUB_REPOSITORY)/EDPS_data
-
-ln -s /home/user/bin/utilities.py /workspaces/$(basename $GITHUB_REPOSITORY)/utilities.py
